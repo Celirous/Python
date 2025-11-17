70875f0e440b5128593af45753aa2454dbd8458c
Celirous <aj.bellringer@gmail.com>
2025-11-17 10:06:35 +0200

    Add files via upload

---
+++ Classwork/Tutorial 3/Chapter 18/learning_logs/urls.py
@@ -0,0 +1,19 @@
+print('\n') 
+
+"""Defines URL Patterns for leaning_logs"""
+
+from django.urls import path
+from .import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+
+    # Home page
+    path('', views.index, name='index'),
+
+    # Page that shows all topics
+    path('topics/', views.topics, name='topics'),
+
+    # Detail page for single topic 
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+]
